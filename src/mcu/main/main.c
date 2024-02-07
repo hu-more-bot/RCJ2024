8264ef5addf24f22b30f0e24b56166dc31cfd364
SamTheDev <samthedev32@gmail.com>
2024-02-07 08:05:14 +0100

    init mcu code

---
+++ src/mcu/main/main.c
@@ -0,0 +1,18 @@
+#include "boards/pico.h"
+#include "pico/stdlib.h"
+
+// TODO: Listen on serial & drive motors
+
+int main() {
+  gpio_init(PICO_DEFAULT_LED_PIN);
+
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  while (true) {
+
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+    sleep_ms(500);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    sleep_ms(500);
+  }
+}
